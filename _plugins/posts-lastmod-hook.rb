178a3bfa0a200be28988f0d0edec7c9e4316a214
Giuseppe Trivisano <giuseppetrivisanogt@gmail.com>
2025-04-25 01:24:54 +0200

    fix(post): risoluzione bug in plugin posts-lastmod-hook

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,11 +4,16 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  # Controlla se il file è tracciato da Git
+  tracked = system("git ls-files --error-unmatch \"#{post.path}\" > /dev/null 2>&1")
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+  if tracked
+    commit_num = `git rev-list --count HEAD "#{post.path}"`.to_i
+
+    if commit_num > 1
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{post.path}"`.strip
+      post.data['last_modified_at'] = lastmod_date
+    end
   end
 
 end
